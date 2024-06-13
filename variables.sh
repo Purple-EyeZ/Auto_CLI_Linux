a0db3e557fa97a32a9c446f94318d3ee682f5104
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2024-06-13 20:33:55 +0200

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,48 @@
+# variables.sh
+
+# CLI
+
+REVANCED_CLI="revanced-cli-4.6.0-all.jar"
+DL_LINK_CLI="https://github.com/ReVanced/revanced-cli/releases/download/v4.6.0/revanced-cli-4.6.0-all.jar"
+
+# Patches
+
+REVANCED_PATCHES="revanced-patches-4.9.0.jar"
+DL_LINK_PATCHES="https://github.com/ReVanced/revanced-patches/releases/download/v4.9.0/revanced-patches-4.9.0.jar"
+
+# integrations
+
+REVANCED_INTEGRATIONS="revanced-integrations-1.10.0.apk"
+DL_LINK_INTEGRATIONS="https://github.com/ReVanced/revanced-integrations/releases/download/v1.10.0/revanced-integrations-1.10.0.apk"
+
+# YouTube
+
+YOUTUBE_VERSION="19.16.39"
+DL_LINK_YOUTUBE="https://pixeldrain.com/api/file/VwwuMCBD?download"
+YOUTUBE_NEW_FILENAME="Youtube_${YOUTUBE_VERSION}.apk"
+
+# YouTube Music V8
+
+YOUTUBE_MUSIC_VERSION="7.03.52"
+DL_LINK_YOUTUBE_MUSIC="https://pixeldrain.com/api/file/M4AhtCJ9?download"
+YOUTUBE_MUSIC_NEW_FILENAME="Youtube_Music_ARMv8_${YOUTUBE_MUSIC_VERSION}.apk"
+
+# YouTube Music V7
+
+DL_LINK_YOUTUBE_MUSIC_V7="https://pixeldrain.com/api/file/zzivkTvP?download"
+YOUTUBE_MUSIC_NEW_FILENAME_V7="Youtube_Music_ARMv7_${YOUTUBE_MUSIC_VERSION}.apk"
+
+# TikTok
+
+TIKTOK_VERSION="32.5.3"
+DL_LINK_TIKTOK="https://pixeldrain.com/api/file/MHETCSGc?download"
+TIKTOK_NEW_FILENAME="TikTok_${TIKTOK_VERSION}.apk"
+
+# Reddit
+
+REDDIT_VERSION="2024.17.0"
+DL_LINK_REDDIT="https://pixeldrain.com/api/file/QBPbVNFX?download"
+REDDIT_NEW_FILENAME="Reddit_${REDDIT_VERSION}.apk"
+
+# Universal
+UNIVERSAL_APK="Universal_x.x.x.apk"
