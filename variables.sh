35e83aacb6231f0c26609d684e022b934231f70a
Purple-EyeZ <bigup25@gmail.com>
2024-06-15 20:49:04 +0200

    Add HASH

--- variables.sh
+++ variables.sh
@@ -4,50 +4,59 @@
 
 REVANCED_CLI="revanced-cli-4.6.0-all.jar"
 DL_LINK_CLI="https://github.com/ReVanced/revanced-cli/releases/download/v4.6.0/revanced-cli-4.6.0-all.jar"
+HASH_CLI="d48a88fefd40c5df056afb7680fa9424"
 
 # Patches
 
 REVANCED_PATCHES="revanced-patches-4.9.0.jar"
 DL_LINK_PATCHES="https://github.com/ReVanced/revanced-patches/releases/download/v4.9.0/revanced-patches-4.9.0.jar"
+HASH_PATCHES="26bc462215116f7f84767f1d61157f2c"
 
 # integrations
 
 REVANCED_INTEGRATIONS="revanced-integrations-1.10.0.apk"
 DL_LINK_INTEGRATIONS="https://github.com/ReVanced/revanced-integrations/releases/download/v1.10.0/revanced-integrations-1.10.0.apk"
+HASH_INTEGRATIONS="bcb9efdc9aeb4979290f1407c61854bb"
 
 # YouTube
 
 YOUTUBE_VERSION="19.16.39"
 DL_LINK_YOUTUBE="https://pixeldrain.com/api/file/VwwuMCBD?download"
+HASH_YOUTUBE="f88e762043e7d2725cbf6a1903485af0"
 YOUTUBE_NEW_FILENAME="Youtube_${YOUTUBE_VERSION}.apk"
 
 # YouTube Music V8
 
 YOUTUBE_MUSIC_VERSION="7.03.52"
 DL_LINK_YOUTUBE_MUSIC="https://pixeldrain.com/api/file/M4AhtCJ9?download"
+HASH_YOUTUBE_MUSIC="2351010035b7487fa2f9ec63fc2fc11d"
 YOUTUBE_MUSIC_NEW_FILENAME="Youtube_Music_ARMv8_${YOUTUBE_MUSIC_VERSION}.apk"
 
 # YouTube Music V7
 
 DL_LINK_YOUTUBE_MUSIC_V7="https://pixeldrain.com/api/file/zzivkTvP?download"
+HASH_YOUTUBE_MUSIC_V7="4f8473e6421768237c07a3facd20df53"
 YOUTUBE_MUSIC_NEW_FILENAME_V7="Youtube_Music_ARMv7_${YOUTUBE_MUSIC_VERSION}.apk"
 
 # TikTok
 
 TIKTOK_VERSION="32.5.3"
 DL_LINK_TIKTOK="https://pixeldrain.com/api/file/MHETCSGc?download"
+HASH_TIKTOK="92d69033e1639daa96742cc70985e462"
 TIKTOK_NEW_FILENAME="TikTok_${TIKTOK_VERSION}.apk"
 
 # Reddit
 
 REDDIT_VERSION="2024.17.0"
 DL_LINK_REDDIT="https://pixeldrain.com/api/file/QBPbVNFX?download"
+HASH_REDDIT="1bf29427a7ca1f2e9f36205755436620"
 REDDIT_NEW_FILENAME="Reddit_${REDDIT_VERSION}.apk"
 
 # Twitter
 
 TWITTER_VERSION="10.41.0"
 DL_LINK_TWITTER="https://pixeldrain.com/api/file/cj62HpCs?download"
+HASH_TWITTER="ec761657192ddaf3bc447765111ad7f9"
 TWITTER_NEW_FILENAME="Twitter_${TWITTER_VERSION}.apk"
 
 # Universal
